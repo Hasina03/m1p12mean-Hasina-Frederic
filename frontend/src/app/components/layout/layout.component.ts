cd4bc2ad64a7b473d25a2328a687fb665f57389e
fredericbob <lalafrederic05@gmail.com>
2025-04-04 15:02:46 +0300

    mecanicien

--- frontend/src/app/components/layout/layout.component.ts
+++ frontend/src/app/components/layout/layout.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+
+
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
